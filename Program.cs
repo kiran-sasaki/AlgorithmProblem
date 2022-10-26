67d82f299cd5fe36a077b88aaa3ee6c578e32887
kiran-sasaki <108934828+kiran-sasaki@users.noreply.github.com>
2022-10-26 12:06:09 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            //BubbleSort bubbleSort = new BubbleSort();
-            //bubbleSort.Sorting();
-            //InsertionSort insertionSort = new InsertionSort();
-            //insertionSort.CheckSorting();
-            //Anagram anagram = new Anagram();
-            //anagram.checkAnagram();
-            //PrimeNumber primeNumber = new PrimeNumber();
-            //primeNumber.PrimeCheck();
-            //BinarySearch binarySearch = new BinarySearch();
-            //BinarySearch.BinarySearchInput();
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.Sorting();
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.CheckSorting();
+            Anagram anagram = new Anagram();
+            anagram.checkAnagram();
+            PrimeNumber primeNumber = new PrimeNumber();
+            primeNumber.PrimeCheck();
+            BinarySearch binarySearch = new BinarySearch();
+            BinarySearch.BinarySearchInput();
             MergeSort mergeSort = new MergeSort();
             mergeSort.MergeSortInput();
         }
