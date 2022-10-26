0678200c1a7c6f64c9a0b4507155579eb165df3d
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-26 12:05:15 +0530

    Added main Method

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgorithmPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.Sorting();
+            //InsertionSort insertionSort = new InsertionSort();
+            //insertionSort.CheckSorting();
+            //Anagram anagram = new Anagram();
+            //anagram.checkAnagram();
+            //PrimeNumber primeNumber = new PrimeNumber();
+            //primeNumber.PrimeCheck();
+            //BinarySearch binarySearch = new BinarySearch();
+            //BinarySearch.BinarySearchInput();
+            MergeSort mergeSort = new MergeSort();
+            mergeSort.MergeSortInput();
+        }
+    }
+}
