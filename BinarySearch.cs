4b5c5c07eb65f2103f3f1a28c6c7db19ad2f9a36
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-26 11:59:37 +0530

    Added BinarySearch Algorithm

---
+++ BinarySearch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public static int BinarySearchDisplay(string[] arr, string x)
+        {
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+
+                int res = x.CompareTo(arr[m]);
+
+                // Check if x is present at mid
+                if (res == 0)
+                    return m;
+
+                // If x greater, ignore left half
+                if (res > 0)
+                    l = m + 1;
+
+                // If x is smaller, ignore right half
+                else
+                    r = m - 1;
+            }
+
+            return -1;
+        }
+        public static void BinarySearchInput()
+        {
+            string[] arr = { "Apple", "Man united", "Ferrari", "India" };
+            string x = "Apple";
+            int result= BinarySearchDisplay(arr, x);
+            if (result == -1)
+            {
+                Console.WriteLine("Element is NOt found");
+            }
+            else
+                Console.WriteLine("Element is found at" + " index " + result);
+        }
+    }
+}
